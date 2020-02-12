1c1422739b4d71c12229c2986936ba238191320f
Edy Segura <edysegura@gmail.com>
2020-02-11 22:29:08 -0300

    Add precache implementation

---
+++ src/sw.js
@@ -0,0 +1,14 @@
+const cacheName = 'picture-chage'
+
+async function precache() {
+  const cache = await caches.open(cacheName)
+  return cache.addAll([
+    './',
+    './index.html',
+    './index.js'
+  ])
+}
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(precache().then(() => self.skipWaiting()))
+})
