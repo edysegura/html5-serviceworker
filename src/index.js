923d4e37dae86ce0a7bab9105d365def05865c6c
Edy Segura <edysegura@gmail.com>
2020-02-14 21:10:19 -0300

    Add JavaScript baseline to register sw.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+class Index {
+
+  constructor() {
+    this.registerServiceWorker()
+  }
+
+  registerServiceWorker() {
+    console.log('Registring Service Worker...')
+  }
+
+}
+
+new Index()
