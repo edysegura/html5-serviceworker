1a44c8df23efa7badfa6c046f1a612fd837c9633
Edy Segura <edysegura@gmail.com>
2020-02-14 21:32:38 -0300

    Add service worker registration

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ class Index {
 
   registerServiceWorker() {
     console.log('Registring Service Worker...')
+
+    const success = () => console.log('Service registration successful')
+    const failure = () => console.log('Service registration failed')
+
+    navigator.serviceWorker
+      .register('./sw.js')
+      .then(success)
+      .catch(failure)
   }
 
 }
