6375d1cb466c174cde3d64d9480fd5bdf76f8bbc
Edy Segura <edysegura@gmail.com>
2024-07-19 21:40:21 -0300

    fix: service worker example

--- src/index.js
+++ src/index.js
@@ -1,43 +1,39 @@
-'use strict'
+"use strict";
 
 class Index {
-
   constructor() {
-    this.registerServiceWorker()
-    this.bindFormAction()
+    this.registerServiceWorker();
+    this.bindFormAction();
   }
 
   fetchImage(imageNumber) {
-    console.log('Image number: ' + imageNumber)
-    const image = document.querySelector('img')
-    image.src = `http://lorempixel.com/500/500/animals/${imageNumber}`
+    console.log("Image number: " + imageNumber);
+    const image = document.querySelector("img");
+    image.src = `https://picsum.photos/id/${imageNumber}/500/500`;
   }
 
   bindFormAction() {
-    const form = document.querySelector('form')
-    const input = form.imageId
+    const form = document.querySelector("form");
+    const input = form.imageId;
 
-    input.select()
+    input.select();
 
-    form.addEventListener('submit', (event) => {
-      event.preventDefault()
-      this.fetchImage(input.value)
-      input.select()
-    })
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      this.fetchImage(input.value);
+      input.select();
+    });
   }
 
   registerServiceWorker() {
-    console.log('[Service Worker] registering sw.js ...')
+    console.log("[Service Worker] registering sw.js ...");
 
-    const success = () => console.log('[Service Worker] registration successful')
-    const failure = () => console.log('[Service Worker] registration failed')
+    const success = () =>
+      console.log("[Service Worker] registration successful");
+    const failure = () => console.log("[Service Worker] registration failed");
 
-    navigator.serviceWorker
-      .register('./sw.js')
-      .then(success)
-      .catch(failure)
+    navigator.serviceWorker.register("./sw.js").then(success).catch(failure);
   }
-
 }
 
-new Index()
+new Index();
