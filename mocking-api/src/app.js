aed9dd6255c105b7d2b067fa122aebd44083b55c
Edy Segura <edysegura@gmail.com>
2025-11-10 17:12:20 -0300

    feat: fetch data from github

--- mocking-api/src/app.js
+++ mocking-api/src/app.js
@@ -1,4 +1,12 @@
 const button = document.querySelector('button');
-button.addEventListener('click', () => {
+button.addEventListener('click', async () => {
   console.log(`👁️ [app.js] button clicked`);
+  console.log(await getUserProfile());
 });
+
+async function getUserProfile(username = 'edysegura') {
+  const endpoint = `https://api.github.com/users/${username}`;
+  const response = await fetch(endpoint);
+  if (response.ok) return response.json();
+  return null;
+}
