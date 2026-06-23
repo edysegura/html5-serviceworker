a181e7d807303db9322cc80af7e5a3daa04d6a11
Edy Segura <edysegura@gmail.com>
2026-06-23 19:08:34 -0300

    feat: implement SW to provide a fallback image

---
+++ image-fallback/sw.js
@@ -0,0 +1,60 @@
+const cacheName = 'image-fallback-v2';
+
+const preCachedAssets = ['./', './index.html', './fallback.webp'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(preCachedAssets)),
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== cacheName)
+            .map((key) => caches.delete(key)),
+        ),
+      ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetchFromCache(event.request));
+});
+
+async function fetchFromCache(request) {
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(request);
+
+  if (cachedResponse) return cachedResponse;
+
+  try {
+    const response = await fetch(request);
+
+    if (
+      !response.ok &&
+      response.type !== 'opaque' &&
+      request.destination === 'image'
+    ) {
+      return cache.match('./fallback.webp');
+    }
+
+    if (response.ok || response.type === 'opaque') {
+      cache.put(request, response.clone());
+    }
+    return response;
+  } catch {
+    if (request.destination === 'image') {
+      return cache.match('./fallback.webp');
+    }
+
+    throw new Error(`Unable to fetch ${request.url}`);
+  }
+}
